66554635d4d044cd4d81f64f75dd8b9ebc526dc0
Blaine Curren <blaine.curren@gmail.com>
2022-10-05 18:23:48 -0500

    added register routes to register.jsx in pages

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  console.log(inputs);
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
         <button>Register</button>
         <p>Error!</p>
         <span>
-          Don't you have an account? <Link to="/login">Login</Link>
+          Do you have an account? <Link to="/login">Login</Link>
         </span>
       </form>
     </div>
