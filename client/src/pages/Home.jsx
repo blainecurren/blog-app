1600d98a1a3736943dade196f7b0df06655d4c67
Blaine Curren <blaine.curren@gmail.com>
2022-10-08 22:28:09 -0500

    added useEffect function to home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
+  const cat = useLocation().search;
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("/posts");
+        const res = await axios.get(`/posts${cat}`);
         setPosts(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-  }, []);
+  }, [cat]);
   // const posts = [
   //   {
   //     id: 1,
